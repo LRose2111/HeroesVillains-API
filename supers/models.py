d459e28320973e4bcc2b3cc3d06a1127f98282bc
Lakota Rose <itsrosebro@gmail.com>
2022-04-05 09:53:15 -0500

    Added Supers, and Super Types to Projects files in My SQL.

--- supers/models.py
+++ supers/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from super_types.models import Type
 
-# Create your models here.
+class Super(models.Model):
+    name = models.CharField(max_length=20)
+    ability = models.CharField(max_length=255)
+    super_type = models.ForeignKey(Type, on_delete=models.CASCADE)
