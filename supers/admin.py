909a292a3c90e249076d33dd4e146d62215cf51a
Lakota Rose <itsrosebro@gmail.com>
2022-04-05 11:01:28 -0500

    Updated Super app files (admin, model, serilizers.

--- supers/admin.py
+++ supers/admin.py
@@ -1,1 +1,6 @@
 from django.contrib import admin
+from .models import Super
+from super_types.models import Type 
+
+admin.site.register(Super)
+admin.site.register(Type)
--- supers/models.py
+++ supers/models.py
@@ -3,5 +3,8 @@ from super_types.models import Type
 
 class Super(models.Model):
     name = models.CharField(max_length=20)
-    ability = models.CharField(max_length=255)
+    alter_ego = models.CharField(max_length=255)
+    primary_ability = models.CharField(max_length=255)
+    secondary_ability = models.CharField(max_length=255)
+    catchphrase = models.CharField(max_length=255)
     super_type = models.ForeignKey(Type, on_delete=models.CASCADE)
